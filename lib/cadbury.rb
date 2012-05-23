a98dc92d3f4be759ede32d8e2a78399b9c7ee3d4
hemanth <hemanth.hm@gmail.com>
2012-05-23 16:06:57 +0530

    Added lib

---
+++ lib/cadbury.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+#!/usr/bin/env ruby
+require 'colored'
+class Cadbury
+  def self.gems(count=10)
+    gem="•"
+    count.times do
+      print gem.send(gem.colors.sample)
+    end
+    puts
+  end
+end
+
+Cadbury.gems()
